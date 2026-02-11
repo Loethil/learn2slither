5ad6b22accb64b5497668e87bbcac355b228f419
Mathis <mathisbatteux2001@gmail.com>
2026-02-11 14:15:06 +0100

    Add red and green apple mechanic

--- src/classes/game.py
+++ src/classes/game.py
@@ -18,38 +18,56 @@ class Game:
         self.winCondition = winCondition
         pygame.init()
         self._running = True
-        self.size = self.weight, self.height = 640, 400
-        self.screen = pygame.display.set_mode(self.size)
+        self.screen = pygame.display.set_mode(WINDOW_SIZE)
 
+
+    def onMoved(self, dir: tuple[int, int]) -> None:
+        nextX = self.env.snake.snakeBody[0].x + dir[0]
+        nextY = self.env.snake.snakeBody[0].y + dir[1]
+        if self.env.snake.length > 1:
+            if nextX == self.env.snake.snakeBody[1].x and nextY == self.env.snake.snakeBody[1].y:
+                return
+        self.env.snake.advance(dir)
+        if self.env.board[nextY, nextX] == 'W' or self.env.board[nextY, nextX] == 'S':
+            print("YOU LOSE! GAME OVER")
+            self.onCleanup()
+        elif self.env.board[nextY, nextX] == 'G':
+            self.env.snake.grow(self.env.addBodypartOnBoard(self.env.snake.snakeBody[-1]))
+            self.env.addAppleOnBoard('G')
+            if self.env.snake.length >= self.winCondition:
+                print("YOU WIN ! CONGRATULATIONS")
+                self.onCleanup()
+        elif self.env.board[nextY, nextX] == 'R':
+            self.env.snake.shrink()
+            self.env.addAppleOnBoard('R')
+            if self.env.snake.length == 0:
+                print("YOU LOSE! GAME OVER")
+                self.onCleanup()
+        
 
     def onEvent(self, event) -> None:
         if event.type == pygame.QUIT:
             self._running = False
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
-                self.env.snake.advance((0, -1))
+                self.onMoved((0, -1))
             elif event.key == pygame.K_DOWN:
-                self.env.snake.advance((0, 1))
+                self.onMoved((0, 1))
             elif event.key == pygame.K_RIGHT:
-                self.env.snake.advance((1, 0))
+                self.onMoved((1, 0))
             elif event.key == pygame.K_LEFT:
-                self.env.snake.advance((-1, 0))
+                self.onMoved((-1, 0))
             self.env.refreshBoard()
-        if self.env.snake.length >= self.winCondition:
-            print("YOU WIN ! CONGRATULATIONS !")
-            exit()
-        if self.env.snake.length == 0:
-            print('GAME OVER')
-            exit()
-
+            printBoard(self.env.board)
 
     def onCleanup(self) -> None:
         pygame.quit()
+        exit()
 
 
     def onExecute(self) -> None:
-        printBoard(self.env.board)
         # self.snake.printVision()
+        printBoard(self.env.board)
         while(self._running):
             for event in pygame.event.get():
                 self.onEvent(event)
