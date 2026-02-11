2496c124d3115b8fb1c25a6411211f17235eb403
Mathis <mathisbatteux2001@gmail.com>
2026-02-11 13:12:03 +0100

    Game file was not saved

--- src/classes/game.py
+++ src/classes/game.py
@@ -1,6 +1,6 @@
-from classes.snake import Snake
-from classes.environment import Env
 import pygame
+from classes.environment import Env
+from renderer import drawGrid, printBoard
 
 CELL_SIZE = 50
 GRID_WIDTH = 32
@@ -14,9 +14,7 @@ clock = pygame.time.Clock()
 class Game:
     def __init__(self, boardXLength: int, boardYLength: int, snakeLength: int, winCondition: int) -> None:
         """"""
-        self.env = Env(boardXLength, boardYLength)
-        self.snakeLength = snakeLength
-        self.snake = Snake(self.env, self.snakeLength)
+        self.env = Env(boardXLength, boardYLength, snakeLength)
         self.winCondition = winCondition
         pygame.init()
         self._running = True
@@ -29,17 +27,18 @@ class Game:
             self._running = False
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
-                self.snake.mouvmentEvent("up")
+                self.env.snake.advance((0, -1))
             elif event.key == pygame.K_DOWN:
-                self.snake.mouvmentEvent("down")
+                self.env.snake.advance((0, 1))
             elif event.key == pygame.K_RIGHT:
-                self.snake.mouvmentEvent("right")
+                self.env.snake.advance((1, 0))
             elif event.key == pygame.K_LEFT:
-                self.snake.mouvmentEvent("left")
-        if self.snake.length >= self.winCondition:
+                self.env.snake.advance((-1, 0))
+            self.env.refreshBoard()
+        if self.env.snake.length >= self.winCondition:
             print("YOU WIN ! CONGRATULATIONS !")
             exit()
-        if self.snake.length == 0:
+        if self.env.snake.length == 0:
             print('GAME OVER')
             exit()
 
@@ -49,37 +48,13 @@ class Game:
 
 
     def onExecute(self) -> None:
-        self.env.printBoard()
-        self.snake.printVision()
+        printBoard(self.env.board)
+        # self.snake.printVision()
         while(self._running):
             for event in pygame.event.get():
                 self.onEvent(event)
             self.screen.fill((0, 0, 0))
-            self.drawGrid(self.screen, self.env.board)
+            drawGrid(self.screen, self.env.board, CELL_SIZE)
             pygame.display.flip()
             clock.tick(10)
         self.onCleanup()
-
-    
-    def drawGrid(self, screen, grid) -> None:
-        for y, row in enumerate(grid):
-            for x, cell in enumerate(row):
-                color = [0, 0, 0]
-                if cell == 'W':
-                    color = [255, 255, 255] # Wall
-                elif cell == 'H':
-                    color = [123, 132, 0] # Snake head
-                elif cell == 'S':
-                    color = [0, 0, 255] # Snake body
-                elif cell == 'G':
-                    color = [0, 255, 0] # Green apple
-                elif cell == 'R':
-                    color = [255, 0, 0] # Red apple
-
-                rect = pygame.Rect(
-                    x * CELL_SIZE,
-                    y * CELL_SIZE,
-                    CELL_SIZE,
-                    CELL_SIZE
-                )
-                pygame.draw.rect(screen, color, rect)
