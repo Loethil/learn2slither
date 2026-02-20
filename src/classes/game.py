6d4e91957e67223c7a5a78d657b22dea0171b7e2
Mathis <mathisbatteux2001@gmail.com>
2026-02-20 11:23:54 +0100

    WinOrLoseConditions now reset the game instead of closing it for agent purpose, fix bug when reseting snake might be stuck in a wall

--- src/classes/game.py
+++ src/classes/game.py
@@ -13,6 +13,9 @@ clock = pygame.time.Clock()
 
 class Game:
     def __init__(self, boardXLength: int, boardYLength: int, snakeLength: int, winCondition: int) -> None:
+        self.boardXLength = boardXLength
+        self.boardYLength = boardYLength
+        self.snakeLength = snakeLength
         self.env = Env(boardXLength, boardYLength, snakeLength)
         self.winCondition = winCondition
         pygame.init()
@@ -34,49 +37,48 @@ class Game:
 
 
     def onEvent(self, event) -> None:
+        snakeMeal: str = ""
         if event.type == pygame.QUIT:
             self._running = False
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
-                self.onMoved((0, -1))
+                snakeMeal = self.onMoved((0, -1))
             elif event.key == pygame.K_DOWN:
-                self.onMoved((0, 1))
+                snakeMeal = self.onMoved((0, 1))
             elif event.key == pygame.K_RIGHT:
-                self.onMoved((1, 0))
+                snakeMeal = self.onMoved((1, 0))
             elif event.key == pygame.K_LEFT:
-                self.onMoved((-1, 0))
+                snakeMeal = self.onMoved((-1, 0))
+            self.checkWinLoseConditions(snakeMeal)
             self.env.refreshBoard()
             self.env.snake.vision = self.env.getSnakeVision()
             printBoard(self.env.board)
             printSnakeVision(self.env.snake.vision)
 
 
-    def onMoved(self, dir: tuple[int, int]) -> None:
+    def onMoved(self, dir: tuple[int, int]) -> str:
         nextX = self.env.snake.snakeBody[0].x + dir[0]
         nextY = self.env.snake.snakeBody[0].y + dir[1]
         if self.env.snake.length > 1:
             if nextX == self.env.snake.snakeBody[1].x and nextY == self.env.snake.snakeBody[1].y:
                 return
-        if self.env.board[nextY, nextX] == 'W' or self.env.board[nextY, nextX] == 'S':
-            print("YOU LOSE! GAME OVER")
-            self.onCleanup()
-        elif self.env.board[nextY, nextX] == 'G':
+        if self.env.board[nextY, nextX] == 'G':
             self.env.snake.grow(self.env.addBodypartOnBoard(self.env.snake.snakeBody[-1]))
             self.env.addAppleOnBoard('G')
         elif self.env.board[nextY, nextX] == 'R':
             self.env.snake.shrink()
             self.env.addAppleOnBoard('R')
-        self.checkWinLoseConditions()
         self.env.snake.advance(dir)
+        return (self.env.board[nextY, nextX])
 
 
-    def checkWinLoseConditions(self) -> None:
-        if self.env.snake.length >= self.winCondition:
-            print("YOU WIN ! CONGRATULATIONS")
-            self.onCleanup()
-        elif self.env.snake.length == 0:
-            print("YOU LOSE! GAME OVER")
-            self.onCleanup()
+    def checkWinLoseConditions(self, snakeMeal: str) -> None:
+        if snakeMeal == 'W' or snakeMeal == 'S' or self.env.snake.length >= self.winCondition or self.env.snake.length == 0:
+            self.resetGame()
+
+
+    def resetGame(self) -> None:
+        self.env = Env(self.boardXLength, self.boardYLength, self.snakeLength)
 
 
     def onCleanup(self) -> None:
